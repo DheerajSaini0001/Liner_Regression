7b824d25f1949fbbd7a4bc28d849cfdaa02d5c03
Dheeraj Saini <dheeraj@192.168.1.31>
2025-06-17 16:32:34 +0530

    Liner

--- Liner.py
+++ Liner.py
@@ -8,11 +8,26 @@ import pandas as pd
 age=np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
 speed=np.array([180,135,172,128,125,110,158,155,152,90,147,145,70,140,138])
 
-slope, interception, r ,p ,Std_error=st.linregress(age,speed)
+# x = [89,43,36,36,95,10,66,34,38,20,26,29,48,64,6,5,36,66,72,40]
+# y = [21,46,3,35,67,95,53,72,58,10,26,34,90,33,38,20,56,2,47,15]
 
+slope, interception, r ,p ,Std_error=st.linregress(age,speed)
 def fun(age):
-    return slope*age + interception
+        return slope*age + interception
 mymodel=list(map(fun,age))
 plt.scatter(age,speed)
 plt.plot(age,mymodel)
+plt.xlabel("Age of Car")
+plt.ylabel("Speed of Car")
+plt.title("Graph Showing the RelationShip Between Speed and Age of Car")
+'''
+slope1, interception1, r1 ,p1 ,Std_error1=st.linregress(x,y)
+def func1(x):
+        return slope1*x+interception1
+mymodel1=list(map(func1,x))
+plt.scatter(x,y)
+plt.plot(x,mymodel1)
+'''
+print("Relationship Between age ang Speed of Car =",r)
+# plt.legend()
 plt.show()
